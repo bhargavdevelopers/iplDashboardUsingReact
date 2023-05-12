2664ac60ffeb4a6261ad469a118fcb8dc8fc00b2
Bhargav <gbhargav.engineer@gmail.com>
2023-05-12 10:42:23 +0000

    ipl dashboard by react route

---
+++ src/components/MatchCard/index.js
@@ -0,0 +1,23 @@
+import './index.css'
+
+const MatchCard = props => {
+  const {matchDetails} = props
+  const {competingTeamLogo, competingTeam, result, matchStatus} = matchDetails
+  const status =
+    matchStatus === 'Won' ? 'match-card-status1' : 'match-card-status'
+
+  return (
+    <li className="match-card-list">
+      <img
+        src={competingTeamLogo}
+        alt={`competing team ${competingTeam}`}
+        className="match-card-image"
+      />
+      <p className="match-card-heading">{competingTeam}</p>
+      <p className="match-card-text">{result}</p>
+      <p className={status}>{matchStatus}</p>
+    </li>
+  )
+}
+
+export default MatchCard
---
+++ src/components/TeamCard/index.js
@@ -0,0 +1,18 @@
+import {Link} from 'react-router-dom'
+import './index.css'
+
+const TeamCard = props => {
+  const {teamDetails} = props
+  const {id, name, teamImageUrl} = teamDetails
+
+  return (
+    <li className="list-container">
+      <Link to={`/team-matches/${id}`} className="link-style">
+        <img src={teamImageUrl} alt={name} className="team-image" />
+        <p className="ipl-name">{name}</p>
+      </Link>
+    </li>
+  )
+}
+
+export default TeamCard
